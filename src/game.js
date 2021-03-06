d02d0199eac4e1259400072e45133fe4c26747a6
Sergey Oganezov <oganez09@gmail.com>
2021-03-06 16:22:55 +0300

    Added shape movement

--- src/game.js
+++ src/game.js
@@ -22,15 +22,75 @@ export default class Game {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   ];
+
   activePiece = {
     x: 0,
     y: 0,
     blocks: [
       [0, 1, 0],
       [1, 1, 1],
-      [0, 0, 0]
-    ]
+      [0, 0, 0],
+    ],
   };
+
+  movePieceLeft() {
+    this.activePiece.x--;
+
+    if (this.hasCollision()) {
+      this.activePiece.x++;
+    }
+  }
+
+  movePieceRight() {
+    this.activePiece.x++;
+
+    if (this.hasCollision()) {
+      this.activePiece.x--;
+    }
+  }
+
+  movePieceDown() {
+    this.activePiece.y++;
+
+    if (this.hasCollision()) {
+      this.activePiece.y--;
+      this.lockPiece();
+    }
+  }
+
+  hasCollision() {
+    const {
+      x: pieceX,
+      y: pieceY,
+      blocks
+    } = this.activePiece;
+
+    for (let y = 0; y < blocks.length; y++) {
+      for (let x = 0; x < blocks[y].length; x++) {
+        if (blocks[y][x] && ((this.playfield[pieceY + y] === undefined || this.playfield[pieceY + y][pieceX + x] === undefined) || this.playfield[pieceY + y][pieceX + x])) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  lockPiece() {
+    const {
+      x: pieceX,
+      y: pieceY,
+      blocks
+    } = this.activePiece;
+
+    for (let y = 0; y < blocks.length; y++) {
+      for (let x = 0; x < blocks[y].length; x++) {
+        if (blocks[y][x]) {
+          this.playfield[pieceY + y][pieceX + x] = blocks[y][x];
+        }
+      }
+    }
+  }
 }
